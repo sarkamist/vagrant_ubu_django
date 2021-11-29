6a56b67ee0f851fb0aacb236b631ec8ffcc59e07
sarkamist <alex_belpo@hotmail.com>
2021-11-29 18:41:54 +0100

    Added DATABASE_URL to env

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,10 +84,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    'default': env.db()
 }
 
 
